f4861d759445c5a0c044987ea36828c6b5c43a74
ZarNiHK <90494561+ZarNiHK@users.noreply.github.com>
2022-02-16 17:26:20 +0630

    login attempt error

--- login.php
+++ login.php
@@ -1,4 +1,31 @@
 <?php include('header.php');?>
+<?php
+    $atmp = 0;
+if (isset($_POST['login'])){
+    $email = $_POST['email'];
+    $password = $_POST['password'];
+    include ("dbconfig.php");
+    $atmp = $_POST['hidden'];
+    if($atmp<3){
+    $query  = "SELECT * FROM users WHERE `email`='".$email."' AND `password`='".$password."'";
+    $result = mysqli_query ($conn, $query);
+    if($query){
+        if (mysqli_num_rows($result)){
+            while (mysqli_fetch_array($result)) {
+                echo "<script> alert('You are logged in Successfully!'); window.location = 'userindex.php'; </script>";
+            }
+        }
+        else{
+            $atmp++;
+            echo '<script> alert("You have invalid username/password and the number of attempt is '. $atmp .'");window.location = "login.php";</script>';
+        }
+    }    
+}
+if ($atmp==3) {
+    echo '<script> alert("You have invalid username/password!");window.location = "register.php";</script>';
+  }
+ }
+?>
 
   <div class="container">
         <div class="row">
